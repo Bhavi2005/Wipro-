237b8acc038a415bb5202ba963e5302eaa506771
Bhavi2005 <136918699+Bhavi2005@users.noreply.github.com>
2025-08-04 10:14:20 +0530

    Add files via upload

---
+++ Lab4.java
@@ -0,0 +1,16 @@
+package Pack1;
+
+public class Lab4 {
+
+	public static void main(String[] args) {
+		System.out.println("Personal Details:");
+		System.out.println("_________________");
+		System.out.println("");
+		System.out.println("First Name:BHAVITHA");
+		System.out.println("Last Name:KAMATHAM");
+		System.out.println("Gender:F");
+		System.out.println("Age :21");
+		System.out.println("Weight:50");
+	}
+
+}
